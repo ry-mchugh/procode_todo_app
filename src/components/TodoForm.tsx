66780d1ab29ce9f215afe7b7303498ffacb2c1f7
Ryan McHugh <41973210+SyntheticDBX@users.noreply.github.com>
2023-06-05 13:53:24 +0100

    create form for adding items to todo list

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,5 +1,40 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { addTodo } from "../features/todo/todosSlice";
+import { addTodo } from "../features/todo/todoSlice";
 import { Button, TextField } from "@material-ui/core";
 
+const TodoForm: React.FC = () => {
+  const dispatch = useDispatch();
+  const [todoText, setTodoText] = useState("");
+  const [error, setError] = useState("");
+
+  const handleAddTodo = () => {
+    if (todoText.trim() === "") {
+      setError("Please enter a valid TODO item.");
+      return; 
+    }
+
+    dispatch(addTodo(todoText));
+    setTodoText("");
+    setError("");
+  };
+
+  return (
+    <div>
+      <TextField
+        label="Add a TODO"
+        value={todoText}
+        onChange={(e) => setTodoText(e.target.value)}
+        fullWidth
+        variant="outlined"
+        margin="normal"
+      />
+      <Button variant="contained" color="primary" onClick={handleAddTodo}>
+        Add TODO
+      </Button>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default TodoForm;
