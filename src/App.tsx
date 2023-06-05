f192628f384e043a59c9fc035169526008f5144b
Ryan McHugh <41973210+SyntheticDBX@users.noreply.github.com>
2023-06-05 14:11:48 +0100

    removed code from index.tsx and added styling and text to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import TodoForm from "./components/TodoForm";
+import { Container, Typography, Box } from "@material-ui/core";
 
 const App: React.FC = () => {
   return (
-    <div></div>
+    <Container maxWidth="sm" style={{ marginTop: "40px" }}>
+      <Box
+        border={1}
+        borderRadius={8}
+        p={4}
+        bgcolor="#f4f4f4"
+        maxWidth="md"
+        margin="auto"
+        boxShadow="0px 2px 4px rgba(0, 0, 0, 0.25)"
+      >
+        <Typography variant="h5" align="center" gutterBottom>
+          TODO List
+        </Typography>
+      </Box>
+    </Container>
   )
 };
 
