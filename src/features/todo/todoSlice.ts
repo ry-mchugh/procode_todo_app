7dd1a164e9d188a71d1ce1d03f84b4e6bf09ff65
Ryan McHugh <41973210+SyntheticDBX@users.noreply.github.com>
2023-06-05 13:40:31 +0100

    added logic for creating slice

--- src/features/todo/todoSlice.ts
+++ src/features/todo/todoSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayLoadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface Todo {
   id: number;
@@ -10,3 +10,49 @@ interface TodoState {
   todos: Todo[];
   completedTodos: Todo[];
 }
+
+const initialState: TodoState = {
+  todos: [],
+  completedTodos: [],
+};
+
+const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      const newTodo = {
+        id: Date.now(),
+        text: action.payload,
+        completed: false,
+      };
+      state.todos.push(newTodo);
+    },
+    toggleTodo: (state, action: PayloadAction<number>) => {
+      const todo = state.todos.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      if (todo.completed) {
+        state.completedTodos.push(todo);
+        state.todos = state.todos.filter(
+          (todo) => todo.id !== action.payload
+        );
+      } else {
+        state.todos.push(todo);
+        state.completedTodos = state.completedTodos.filter(
+          (todo) => todo.id !== action.payload
+        );
+      }
+    }
+  },
+  deleteTodo: (state, action: PayloadAction<number>) => {
+    state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    state.completedTodos = state.completedTodos.filter(
+      (todo) => todo.id !== action.payload
+    );
+  },
+},
+});
+
+export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions;
+export default todoSlice.reducer;
